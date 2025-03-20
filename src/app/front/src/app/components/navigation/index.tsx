0f37c7d1cd69d0a351653499e7ec6e69bb7d205f
ereszka <e.reszka@stud.elka.pw.edu.pl>
2025-03-20 20:10:00 +0100

    add front app sceleton

---
+++ src/app/front/src/app/components/navigation/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Navbar from "./navbar";
+
+const Navigation = () => {
+  return (
+    <div className="navbar">
+      <Navbar/>
+    </div>
+  );
+};
+
+export default Navigation;
