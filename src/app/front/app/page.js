cda0d02447b7e6ce49b8e41909b593123caa2e6d
Filip <fkobus.coding@gmail.com>
2025-03-22 00:19:28 +0100

    Created simple video upload form

--- src/app/front/app/page.js
+++ src/app/front/app/page.js
@@ -1,9 +1,13 @@
 import myVideo from '/videos/get-started.mp4';
 import HowItWorks from "./components/how-it-works";
+import VideoUpload from "./components/video-upload";
 import Video from 'next-video';
 
 export default function Home() {
   return (
+    <div>
       <HowItWorks />
+      <VideoUpload />
+    </div>
   );
 }
