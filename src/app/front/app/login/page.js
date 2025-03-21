3ead98251f32eb4ae66d576274e270c710575d57
Filip <fkobus.coding@gmail.com>
2025-03-21 21:04:08 +0100

    Added dummy pages (login, about)

---
+++ src/app/front/app/login/page.js
@@ -0,0 +1,9 @@
+export default function Login() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-[calc(80vh-140px)] px-4 py-8">
+        <h1 className="text-3xl font-bold mb-4">Login Page</h1>
+        <p className="text-gray-500">🚧 Login functionality is still TODO 🚧</p>
+      </div>
+    );
+  }
+  
--- src/app/front/app/page.js
+++ src/app/front/app/page.js
@@ -2,7 +2,7 @@ import { geistSans } from "./fonts";
 
 export default function Home() {
   return (
-    <div className="justify-items-center p-8">
+    <div className="flex flex-col items-center justify-center min-h-[calc(80vh-140px)] px-4 py-8">
         <h1 className={`${geistSans.className}`}>Hello World</h1>
     </div>
   );
