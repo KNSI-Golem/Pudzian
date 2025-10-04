51ff4558e85f74f04da5cdd147bd4c16cf4bf80f
Filip <kobusfilip@outlook.com>
2025-10-04 22:17:27 +0200

    chore: remove old frontend, add context for llm in sources

--- src/app/front/app/page.js
+++
@@ -1,9 +0,0 @@
-import VideoSection from "./components/video-section";
-
-export default function Home() {
-  return (
-    <div className="flex justify-center items-center">
-        <VideoSection />
-    </div>
-  );
-}
