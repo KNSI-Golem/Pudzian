76b4779dc68ae85c1de4a0dde90b9b713b3c25e1
ereszka <eva.f.reszka@gmail.com>
2026-06-26 18:08:47 +0200

    Change mapping and config convention

--- src/app/front/src/types/animate.ts
+++ src/app/front/src/types/animate.ts
@@ -1,16 +1,5 @@
 import {PoseDetectionResult} from "@/types/mediapipe";
 
-export interface AnimateProps {
+export type AnimateProps =  {
     poseResultRef: PoseDetectionResult;
-}
-
-export interface AnimateMappingConfig {
-    handLeft: number,
-    handRight: number,
-    foreArmLeft: number,
-    foreArmRight: number,
-    armLeft: number,
-    armRight: number,
-    shoulderLeft: number,
-    shoulderRight: number,
 }
