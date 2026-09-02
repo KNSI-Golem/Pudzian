ffa9be1190eef6d261bb9a3e479e571cd5fafc96
ereszka <eva.f.reszka@gmail.com>
2026-09-02 15:36:38 +0200

    Remove old font from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import {Jura} from "next/font/google";
 import "./globals.css";
 import "@/styles/components.css";
 import { Header, Footer } from "@/components/layout";
 import { APP_CONFIG } from "@/lib/constants";
-
-const jura = Jura({
-  variable: "--font-jura",
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-})
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: `${APP_CONFIG.fullName} ${APP_CONFIG.name}`,
@@ -35,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="pl">
       <body
-        className={`${jura.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
+        className={`antialiased min-h-screen flex flex-col`}
       >
         {children}
         <Footer />
