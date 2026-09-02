660e86bc7347ddf2ddabaa88a9517f73bfab8167
ereszka <eva.f.reszka@gmail.com>
2026-09-02 15:40:24 +0200

    remove redundant flex in flex-grow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { VideoSessionOrchestrator } from '@/components';
 export default function Home() {
 
   return (
-    <main className="flex-grow container mx-auto p-4 md:p-8 flex items-center justify-center">
+    <main className="grow container mx-auto p-4 md:p-8 flex items-center justify-center">
      <VideoSessionOrchestrator/>
     </main>
   );
